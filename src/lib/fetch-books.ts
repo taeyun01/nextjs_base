8f3b384a4ecff9a8378601ff30378abb4a6ab181
taeyun <lwe12f675@gmail.com>
2024-09-02 13:43:17 +0900

    feat: url변경 (CLI로 버셀 배포)

--- src/lib/fetch-books.ts
+++ src/lib/fetch-books.ts
@@ -2,7 +2,7 @@ import { BookData } from "@/types/types";
 
 //* fetchBooks()를 확장해서 검색 기능도 사용할 수 있도록 만들기
 export const fetchBooks = async (q?: string): Promise<BookData[]> => {
-  let url = `http://localhost:12345/book`;
+  let url = `https://onebito-books-server.vercel.app/book`;
 
   if (q) {
     url += `/search?q=${q}`;
--- src/lib/fetch-random-books.ts
+++ src/lib/fetch-random-books.ts
@@ -1,7 +1,7 @@
 import { BookData } from "@/types/types";
 
 export const fetchRandomBooks = async (): Promise<BookData[]> => {
-  const url = `http://localhost:12345/book/random`;
+  const url = `https://onebito-books-server.vercel.app/book/random`;
 
   try {
     const response = await fetch(url);
